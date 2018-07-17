4b2f169e18ac9fbff849cd6dab872462defcde88
Unknown <devalexdom@gmail.com>
2018-07-17 03:49:27 +0200

    You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
    Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
    
    Kata.Likes(new string[0]) => "no one likes this"
    Kata.Likes(new string[] {"Peter"}) => "Peter likes this"
    Kata.Likes(new string[] {"Jacob", "Alex"}) => "Jacob and Alex like this"
    Kata.Likes(new string[] {"Max", "John", "Mark"}) => "Max, John and Mark like this"
    Kata.Likes(new string[] {"Alex", "Jacob", "Mark", "Max"}) => "Alex, Jacob and 2 others like this"
    
    For 4 or more names, the number in and 2 others simply increases.

---
+++ Who_likes_it.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Who_likes_it
+{
+    class MainClass
+    {
+        public static string Likes(string[] name)
+        {
+            string output="Unknow error";
+
+            switch (name.Length)
+            {   
+                case 0:
+                    output = "no one likes this";
+                    break;
+                case 1:
+                    output = name[0] + " likes this";
+                    break;
+                case 2:
+                    output = name[0] + " and " + name[1] + " like this";
+                    break;
+                case 3:
+                    output = name[0] + ", " + name[1] + " and " + name[2] + " like this";
+                    break;
+                default:
+                    if (name.Length > 3)
+                    {
+                        output = name[0] + ", " + name[1] + " and " + (name.Length-2) + " others like this";
+                    }
+                    break;
+            };
+            return output;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(MainClass.Likes(new string[] { "Alex", "Jacob", "Mark", "Max" }));
+        }
+    }
+}
