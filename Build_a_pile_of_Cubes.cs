dc1eea73bfb173351323c9c4e83cf8e02c892257
Unknown <devalexdom@gmail.com>
2018-07-17 03:05:18 +0200

    Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
    You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
    
    The parameter of the function findNb (find_nb, find-nb, findNb) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
    Examples:
    
    findNb(1071225) --> 45
    findNb(91716553919377) --> -1

---
+++ Build_a_pile_of_Cubes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exe1
+{
+    class MainClass
+    {
+        public static long findNb(long m) {
+     		long endValue = m,currentValue = 0,n = 1;
+
+            while (currentValue < endValue)
+            {
+                currentValue += (long)System.Math.Pow((n - 1), 3);
+                if (currentValue == endValue)
+                {
+                    return n-1;
+                }
+                else
+                {
+                    n++;
+                }
+
+            }
+
+            return -1;
+  		}
+    
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Exe1 Codewars");
+            Console.WriteLine("Test: "+MainClass.findNb(4183059834009));
+           
+        }
+    }
+}
